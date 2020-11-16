6cb66c70f30ca2b2e3bd802aaa4edd2572a93c99
webfullstack99 <webfullstack99@gmail.com>
2020-11-16 21:59:30 +0700

    chat front-end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,12 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-    public _items: User[] = [];
+
+    public _username: string = '';
+    public _userOnline: any[] = [];
+    public _messages: any[] = [];
     public _form: FormGroup;
+    private _isUsernameDisabled: boolean = false;
 
     constructor(
         private _api: ApiService,
@@ -21,44 +25,57 @@ export class AppComponent implements OnInit {
 
     ngOnInit(): void {
         this.initForm();
-        this.getUsers();
-        this._api.getSocket().on('new-data', function (data: any) {
-            this.getUsers();
+
+        // USER ONLINE
+        this._api.getSocket().on('SERVER_RETURN_USER_ONLINE', function (data: any) {
+            this._userOnline = data;
+        }.bind(this));
+
+        // MESSAGE
+        this._api.getSocket().on('SERVER_RETURN_MESSAGE', function (data: any) {
+            this._messages.push(data);
+            let el = document.getElementById('message-container');
+            if (el) {
+                setTimeout(() => {
+                    el.scrollTop = el.scrollHeight;
+                });
+            }
         }.bind(this));
     }
 
     private initForm(): void {
         this._form = this._formBuilder.group({
             username: ['', [Validators.required]],
-            email: ['', [Validators.required]],
-            fullName: ['', [Validators.required]],
-        })
-    }
-
-    public getUsers() {
-        this._api.getUsers((items: User[]) => {
-            this._items = items
-        })
-    }
-
-    public onItemClicked(item: User) {
-        this._api.deleteUser(item.getId(), () => {
-            this._api.getSocket().emit('delete-data', null);
+            message: ['', [Validators.required]],
         })
     }
 
     public onSubmit($event) {
         if (this._form.valid) {
-            const { username, email, fullName } = this._form.value;
-            let user: User = new User(null, username, email, '123456', fullName);
-            this._api.addUser(user, () => {
-                this._api.getSocket().emit('new-data', null);
-            });
-            this._form.reset();
+            const { username, message } = this._form.value;
+            if (username) this._username = username;
+            this._form.controls.username.disable();
+            this._form.controls.message.reset();
+            this._api.getSocket().emit('CLIENT_SEND_MESSAGE', { username: this._username, content: message });
         }
     }
 
     public isEnableSubmitBtn(): boolean {
         return this._form.valid;
+    }
+
+    public getIsUsernameDisabled(): boolean {
+        return this._isUsernameDisabled;
+    }
+
+    public setIsUsernameDisabled(value: boolean) {
+        this._isUsernameDisabled = value;
+    }
+
+    public isMine(username: string): boolean {
+        return (username == this._username);
+    }
+
+    public onMessageContainerScrolled($event: Event) {
     }
 }
